74aee85503b3d1d4a3d1214a29f8e3cab10701ec
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-09-14 00:10:19 +0300

    feat(sockets): add leave-session, end-session and list-sessions to the session management events

--- src/sockets/session.socket.js
+++ src/sockets/session.socket.js
@@ -1,4 +1,5 @@
 const GameService = require("../services/game.service");
+const SessionService = require("../services/session.service");
 
 module.exports = (socket, io) => {
   socket.on("create-session", (data) => {
@@ -37,4 +38,44 @@ module.exports = (socket, io) => {
       socket.emit("error", { message: error.message });
     }
   });
+
+  socket.on("leave-session", (data) => {
+    try {
+      const { sessionId } = data;
+      const session = SessionService.leaveSession(sessionId, socket.id);
+
+      socket.leave(sessionId);
+
+      io.to(sessionId).emit("player-left", {
+        playerId: socket.id,
+        playerCount: session.getPlayerCount(),
+        players: session.getPlayersList()
+      })
+    } catch (error) {
+      socket.emit("error", { message: error.message });
+    }
+  });
+
+  socket.on("end-session", (data) => {
+    try {
+      const { sessionId } = data;
+      const session = SessionService.endSession(sessionId, socket.id);
+
+      io.to(sessionId).emit("session-ended", { sessionId });
+
+      // Force all players to leave session room
+      io.in(sessionId).socketsLeave(sessionId);
+    } catch (error) {
+      socket.emit("error", { message: error.message });
+    }
+  });
+
+  socket.on("list-sessions", () => {
+    try {
+      const sessions = SessionService.getAllSessions();
+      socket.emit("sessions-list", { sessions })
+    } catch (error) {
+      socket.emit("error", { message: error.message });
+    }
+  })
 }
